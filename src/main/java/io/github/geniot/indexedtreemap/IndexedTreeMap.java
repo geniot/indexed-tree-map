57bad4e3cb4a2d9b826135f0f363e939dde6a806
Vitaly Sazanovich <vitaly.sazanovich@gmail.com>
2021-06-28 12:20:58 +0200

    Preparing for release to maven.

--- src/main/java/io/github/geniot/indexedtreemap/IndexedTreeMap.java
+++ src/main/java/io/github/geniot/indexedtreemap/IndexedTreeMap.java
@@ -13,8 +13,8 @@ package io.github.geniot.indexedtreemap;
  * ORACLE PROPRIETARY/CONFIDENTIAL. Use is subject to license terms.
  */
 
-import java.util.*;
 import java.util.AbstractSet;
+import java.util.*;
 
 
 /**
@@ -22,12 +22,12 @@ import java.util.AbstractSet;
  * The map is sorted according to the {@linkplain Comparable natural
  * ordering} of its keys, or by a {@link java.util.Comparator} provided at map
  * creation time, depending on which constructor is used.
- * <p/>
+ *
  * <p>This implementation provides guaranteed log(n) time cost for the
  * <tt>containsKey</tt>, <tt>get</tt>, <tt>put</tt> and <tt>remove</tt>
  * operations.  Algorithms are adaptations of those in Cormen, Leiserson, and
  * Rivest's <I>Introduction to Algorithms</I>.
- * <p/>
+ *
  * <p>Note that the ordering maintained by a sorted map (whether or not an
  * explicit comparator is provided) must be <i>consistent with equals</i> if
  * this sorted map is to correctly implement the <tt>Map</tt> interface.  (See
@@ -39,7 +39,7 @@ import java.util.AbstractSet;
  * standpoint of the sorted map, equal.  The behavior of a sorted map
  * <i>is</i> well-defined even if its ordering is inconsistent with equals; it
  * just fails to obey the general contract of the <tt>Map</tt> interface.
- * <p/>
+ *
  * <p><strong>Note that this implementation is not synchronized.</strong>
  * If multiple threads access a map concurrently, and at least one of the
  * threads modifies the map structurally, it <i>must</i> be synchronized
@@ -53,7 +53,7 @@ import java.util.AbstractSet;
  * method.  This is best done at creation time, to prevent accidental
  * unsynchronized access to the map: <pre>
  *   SortedMap m = Collections.synchronizedSortedMap(new IndexedTreeMap(...));</pre>
- * <p/>
+ *
  * <p>The iterators returned by the <tt>iterator</tt> method of the collections
  * returned by all of this class's "collection view methods" are
  * <i>fail-fast</i>: if the map is structurally modified at any time after the
@@ -62,7 +62,7 @@ import java.util.AbstractSet;
  * java.util.ConcurrentModificationException}.  Thus, in the face of concurrent
  * modification, the iterator fails quickly and cleanly, rather than risking
  * arbitrary, non-deterministic behavior at an undetermined time in the future.
- * <p/>
+ *
  * <p>Note that the fail-fast behavior of an iterator cannot be guaranteed
  * as it is, generally speaking, impossible to make any hard guarantees in the
  * presence of unsynchronized concurrent modification.  Fail-fast iterators
@@ -70,13 +70,13 @@ import java.util.AbstractSet;
  * Therefore, it would be wrong to write a program that depended on this
  * exception for its correctness:   <i>the fail-fast behavior of iterators
  * should be used only to detect bugs.</i>
- * <p/>
+ *
  * <p>All <tt>Map.Entry</tt> pairs returned by methods in this class
  * and its views represent snapshots of mappings at the time they were
  * produced. They do <em>not</em> support the <tt>Entry.setValue</tt>
  * method. (Note however that it is possible to change mappings in the
  * associated map using <tt>put</tt>.)
- * <p/>
+ *
  * <p>This class is a member of the
  * <a href="{@docRoot}/../technotes/guides/collections/index.html">
  * Java Collections Framework</a>.
@@ -206,7 +206,7 @@ public class IndexedTreeMap<K, V>
      *
      * @param key key whose presence in this map is to be tested
      * @return <tt>true</tt> if this map contains a mapping for the
-     *         specified key
+     * specified key
      * @throws ClassCastException   if the specified key cannot be compared
      *                              with the keys currently in the map
      * @throws NullPointerException if the specified key is null
@@ -227,7 +227,7 @@ public class IndexedTreeMap<K, V>
      *
      * @param value value whose presence in this map is to be tested
      * @return <tt>true</tt> if a mapping to <tt>value</tt> exists;
-     *         <tt>false</tt> otherwise
+     * <tt>false</tt> otherwise
      * @since 1.2
      */
     public boolean containsValue(Object value) {
@@ -240,13 +240,13 @@ public class IndexedTreeMap<K, V>
     /**
      * Returns the value to which the specified key is mapped,
      * or {@code null} if this map contains no mapping for the key.
-     * <p/>
+     *
      * <p>More formally, if this map contains a mapping from a key
      * {@code k} to a value {@code v} such that {@code key} compares
      * equal to {@code k} according to the map's ordering, then this
      * method returns {@code v}; otherwise it returns {@code null}.
      * (There can be at most one such mapping.)
-     * <p/>
+     *
      * <p>A return value of {@code null} does not <i>necessarily</i>
      * indicate that the map contains no mapping for the key; it's also
      * possible that the map explicitly maps the key to {@code null}.
@@ -269,16 +269,14 @@ public class IndexedTreeMap<K, V>
     }
 
     /**
-     * @throws java.util.NoSuchElementException
-     *          {@inheritDoc}
+     * @throws java.util.NoSuchElementException {@inheritDoc}
      */
     public K firstKey() {
         return key(getFirstEntry());
     }
 
     /**
-     * @throws java.util.NoSuchElementException
-     *          {@inheritDoc}
+     * @throws java.util.NoSuchElementException {@inheritDoc}
      */
     public K lastKey() {
         return key(getLastEntry());
@@ -319,7 +317,7 @@ public class IndexedTreeMap<K, V>
      * does not contain an entry for the key.
      *
      * @return this map's entry for the given key, or <tt>null</tt> if the map
-     *         does not contain an entry for the key
+     * does not contain an entry for the key
      * @throws ClassCastException   if the specified key cannot be compared
      *                              with the keys currently in the map
      * @throws NullPointerException if the specified key is null
@@ -531,9 +529,9 @@ public class IndexedTreeMap<K, V>
      * @param key   key with which the specified value is to be associated
      * @param value value to be associated with the specified key
      * @return the previous value associated with <tt>key</tt>, or
-     *         <tt>null</tt> if there was no mapping for <tt>key</tt>.
-     *         (A <tt>null</tt> return can also indicate that the map
-     *         previously associated <tt>null</tt> with <tt>key</tt>.)
+     * <tt>null</tt> if there was no mapping for <tt>key</tt>.
+     * (A <tt>null</tt> return can also indicate that the map
+     * previously associated <tt>null</tt> with <tt>key</tt>.)
      * @throws ClassCastException   if the specified key cannot be compared
      *                              with the keys currently in the map
      * @throws NullPointerException if the specified key is null
@@ -603,9 +601,9 @@ public class IndexedTreeMap<K, V>
      *
      * @param key key for which mapping should be removed
      * @return the previous value associated with <tt>key</tt>, or
-     *         <tt>null</tt> if there was no mapping for <tt>key</tt>.
-     *         (A <tt>null</tt> return can also indicate that the map
-     *         previously associated <tt>null</tt> with <tt>key</tt>.)
+     * <tt>null</tt> if there was no mapping for <tt>key</tt>.
+     * (A <tt>null</tt> return can also indicate that the map
+     * previously associated <tt>null</tt> with <tt>key</tt>.)
      * @throws ClassCastException   if the specified key cannot be compared
      *                              with the keys currently in the map
      * @throws NullPointerException if the specified key is null
@@ -2167,7 +2165,7 @@ public class IndexedTreeMap<K, V>
          * value.
          *
          * @return the value associated with the key before this method was
-         *         called
+         * called
          */
         public V setValue(V value) {
             V oldValue = this.value;
@@ -2264,7 +2262,7 @@ public class IndexedTreeMap<K, V>
 
     /**
      * Balancing operations.
-     * <p/>
+     * <p>
      * Implementations of rebalancings during insertion and deletion are
      * slightly different than the CLR version.  Rather than using dummy
      * nilnodes, we use a set of accessors that deal properly with null.  They
@@ -2552,6 +2550,8 @@ public class IndexedTreeMap<K, V>
      * Save the state of the <tt>IndexedTreeMap</tt> instance to a stream (i.e.,
      * serialize it).
      *
+     * @param s input stream
+     * @throws java.io.IOException io exception
      * @serialData The <i>size</i> of the IndexedTreeMap (the number of key-value
      * mappings) is emitted (int), followed by the key (Object)
      * and value (Object) for each key-value mapping represented
@@ -2579,6 +2579,10 @@ public class IndexedTreeMap<K, V>
     /**
      * Reconstitute the <tt>IndexedTreeMap</tt> instance from a stream (i.e.,
      * deserialize it).
+     *
+     * @param s input stream
+     * @throws java.io.IOException io exception
+     * @throws ClassNotFoundException class not found
      */
     private void readObject(final java.io.ObjectInputStream s)
             throws java.io.IOException, ClassNotFoundException {
@@ -2597,15 +2601,19 @@ public class IndexedTreeMap<K, V>
     void readTreeSet(int size, java.io.ObjectInputStream s, V defaultVal)
             throws java.io.IOException, ClassNotFoundException {
         buildFromSorted(size, null, s, defaultVal);
-        if (root!=null){
+        if (root != null) {
             updateWeight(root);
         }
     }
 
-    private int updateWeight(Entry<K,V>e){
+    private int updateWeight(Entry<K, V> e) {
         int weight = 1;
-        if (e.left!=null){weight += updateWeight(e.left);}
-        if (e.right!=null){weight += updateWeight(e.right);}
+        if (e.left != null) {
+            weight += updateWeight(e.left);
+        }
+        if (e.right != null) {
+            weight += updateWeight(e.right);
+        }
         e.weight = weight;
         return weight;
     }
@@ -2627,13 +2635,13 @@ public class IndexedTreeMap<K, V>
      * and/or values from iterator or stream. This leads to too many
      * parameters, but seems better than alternatives.  The four formats
      * that this method accepts are:
-     * <p/>
+     * <p>
      * 1) An iterator of Map.Entries.  (it != null, defaultVal == null).
      * 2) An iterator of keys.         (it != null, defaultVal != null).
      * 3) A stream of alternating serialized keys and values.
      * (it == null, defaultVal == null).
      * 4) A stream of serialized keys. (it == null, defaultVal != null).
-     * <p/>
+     * <p>
      * It is assumed that the comparator of the IndexedTreeMap is already set prior
      * to calling this method.
      *
--- src/main/java/io/github/geniot/indexedtreemap/IndexedTreeSet.java
+++ src/main/java/io/github/geniot/indexedtreemap/IndexedTreeSet.java
@@ -21,10 +21,10 @@ import java.util.*;
  * The elements are ordered using their {@linkplain Comparable natural
  * ordering}, or by a {@link java.util.Comparator} provided at set creation
  * time, depending on which constructor is used.
- * <p/>
+ *
  * <p>This implementation provides guaranteed log(n) time cost for the basic
  * operations ({@code add}, {@code remove} and {@code contains}).
- * <p/>
+ *
  * <p>Note that the ordering maintained by a set (whether or not an explicit
  * comparator is provided) must be <i>consistent with equals</i> if it is to
  * correctly implement the {@code Set} interface.  (See {@code Comparable}
@@ -36,7 +36,7 @@ import java.util.*;
  * are, from the standpoint of the set, equal.  The behavior of a set
  * <i>is</i> well-defined even if its ordering is inconsistent with equals; it
  * just fails to obey the general contract of the {@code Set} interface.
- * <p/>
+ *
  * <p><strong>Note that this implementation is not synchronized.</strong>
  * If multiple threads access a tree set concurrently, and at least one
  * of the threads modifies the set, it <i>must</i> be synchronized
@@ -47,7 +47,7 @@ import java.util.*;
  * method.  This is best done at creation time, to prevent accidental
  * unsynchronized access to the set: <pre>
  *   SortedSet s = Collections.synchronizedSortedSet(new IndexedTreeSet(...));</pre>
- * <p/>
+ *
  * <p>The iterators returned by this class's {@code iterator} method are
  * <i>fail-fast</i>: if the set is modified at any time after the iterator is
  * created, in any way except through the iterator's own {@code remove}
@@ -55,7 +55,7 @@ import java.util.*;
  * Thus, in the face of concurrent modification, the iterator fails quickly
  * and cleanly, rather than risking arbitrary, non-deterministic behavior at
  * an undetermined time in the future.
- * <p/>
+ *
  * <p>Note that the fail-fast behavior of an iterator cannot be guaranteed
  * as it is, generally speaking, impossible to make any hard guarantees in the
  * presence of unsynchronized concurrent modification.  Fail-fast iterators
@@ -63,7 +63,7 @@ import java.util.*;
  * Therefore, it would be wrong to write a program that depended on this
  * exception for its correctness:   <i>the fail-fast behavior of iterators
  * should be used only to detect bugs.</i>
- * <p/>
+ *
  * <p>This class is a member of the
  * <a href="{@docRoot}/../technotes/guides/collections/index.html">
  * Java Collections Framework</a>.
@@ -118,7 +118,7 @@ public class IndexedTreeSet<E> extends java.util.AbstractSet<E>
      * Constructs a new, empty tree set, sorted according to the specified
      * comparator.  All elements inserted into the set must be <i>mutually
      * comparable</i> by the specified comparator: {@code comparator.compare(e1,
-     *e2)} must not throw a {@code ClassCastException} for any elements
+     * e2)} must not throw a {@code ClassCastException} for any elements
      * {@code e1} and {@code e2} in the set.  If the user attempts to add
      * an element to the set that violates this constraint, the
      * {@code add} call will throw a {@code ClassCastException}.
@@ -234,7 +234,7 @@ public class IndexedTreeSet<E> extends java.util.AbstractSet<E>
      *
      * @param e element to be added to this set
      * @return {@code true} if this set did not already contain the specified
-     *         element
+     * element
      * @throws ClassCastException   if the specified object cannot be compared
      *                              with the elements currently in this set
      * @throws NullPointerException if the specified element is null
@@ -475,6 +475,8 @@ public class IndexedTreeSet<E> extends java.util.AbstractSet<E>
      * Save the state of the {@code IndexedTreeSet} instance to a stream (that is,
      * serialize it).
      *
+     * @param s input stream
+     * @throws java.io.IOException io exception
      * @serialData Emits the comparator used to order this set, or
      * {@code null} if it obeys its elements' natural ordering
      * (Object), followed by the size of the set (the number of
@@ -502,6 +504,10 @@ public class IndexedTreeSet<E> extends java.util.AbstractSet<E>
     /**
      * Reconstitute the {@code IndexedTreeSet} instance from a stream (that is,
      * deserialize it).
+     *
+     * @param s input stream
+     * @throws java.io.IOException io exception
+     * @throws ClassNotFoundException class not found
      */
     private void readObject(java.io.ObjectInputStream s)
             throws java.io.IOException, ClassNotFoundException {
